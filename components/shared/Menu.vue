d8d1aa942f17640f4e0ca8e76a207bf8b10cb590
Lee Palacios <vitualizz@gmail.com>
2020-06-09 04:28:54 -0500

    separate menu to component

---
+++ components/shared/Menu.vue
@@ -0,0 +1,54 @@
+<template lang='pug'>
+  .navbar.is-fixed-top.is-transparent
+    a.navbar-burger.burger(
+      @click='activeNavbar'
+    )
+      span(aria-hidden='true')
+      span(aria-hidden='true')
+      span(aria-hidden='true')
+    .navbar-menu
+      .navbar-end
+        nuxt-link(
+          to='/'
+        ).navbar-item Inicio
+        nuxt-link(
+          to='/'
+        ).navbar-item Blog
+        nuxt-link(
+          to='/'
+        ).navbar-item Proyectos
+        nuxt-link(
+          to='/'
+        ).navbar-item Sobre mi
+        nuxt-link(
+          to='/'
+        ).navbar-item Contáctame
+</template>
+
+<script>
+export default {
+  methods: {
+    activeNavbar () {
+      $('.navbar').toggleClass('burguer')
+      $('.navbar-menu').toggleClass('is-active')
+    }
+  }
+}
+</script>
+
+<style scoped lang='sass'>
+.navbar:not(.burguer)
+  background: transparent
+  .navbar-item
+    color: $white
+    font-weight: 400
+    &:hover
+      font-weight: bold
+
+.navbar.burguer
+  .navbar-item
+    color: $black
+    font-weight: 400
+    &:hover
+      font-weight: bold
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,56 +1,5 @@
 <template lang='pug'>
   div
-    .navbar.is-fixed-top.is-transparent
-      a.navbar-burger.burger(
-        @click='activeNavbar'
-      )
-        span(aria-hidden='true')
-        span(aria-hidden='true')
-        span(aria-hidden='true')
-      .navbar-menu
-        .navbar-end
-          nuxt-link(
-            to='/'
-          ).navbar-item Inicio
-          nuxt-link(
-            to='/'
-          ).navbar-item Blog
-          nuxt-link(
-            to='/'
-          ).navbar-item Proyectos
-          nuxt-link(
-            to='/'
-          ).navbar-item Sobre mi
-          nuxt-link(
-            to='/'
-          ).navbar-item Contáctame
+    Menu
     nuxt
 </template>
-
-<script>
-export default {
-  methods: {
-    activeNavbar () {
-      $('.navbar').toggleClass('burguer')
-      $('.navbar-menu').toggleClass('is-active')
-    }
-  }
-}
-</script>
-
-<style scoped lang='sass'>
-.navbar:not(.burguer)
-  background: transparent
-  .navbar-item
-    color: $white
-    font-weight: 400
-    &:hover
-      font-weight: bold
-
-.navbar.burguer
-  .navbar-item
-    color: $black
-    font-weight: 400
-    &:hover
-      font-weight: bold
-</style>
