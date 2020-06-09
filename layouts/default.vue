4501df2070aac5646a156f0665468772f8d220f0
Lee Palacios <vitualizz@gmail.com>
2020-06-09 03:05:09 -0500

    menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,31 @@
-<template>
-  <div>
-    <nuxt />
-  </div>
+<template lang='pug'>
+  div
+    .navbar.is-fixed-top.is-transparent
+      .navbar-end
+        nuxt-link(
+          to='/'
+        ).navbar-item Inicio
+        nuxt-link(
+          to='/'
+        ).navbar-item Blog
+        nuxt-link(
+          to='/'
+        ).navbar-item Proyectos
+        nuxt-link(
+          to='/'
+        ).navbar-item Sobre mi
+        nuxt-link(
+          to='/'
+        ).navbar-item Contáctame
+    nuxt
 </template>
+
+<style scoped lang='sass'>
+.navbar
+  background: transparent
+  .navbar-item
+    color: $white
+    font-weight: 400
+    &:hover
+      font-weight: bold
+</style>
